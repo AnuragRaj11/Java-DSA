f34f86a367bc02eaf08482b20b47c8e9d7878edd
AnuragRaj11 <Araj59519@gmail.com>
2024-08-11 01:28:32 +0530

    ls

--- linearsearch.java
+++ linearsearch.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 public class linearsearch {
     public static void main(String args[]) {
-        int [] arr={1,6,5,4,7};
+        int [] arr={2,6,5,4,7};
         int target=6;
         int ans= ls(arr, target);
         System.out.println(ans);
+        System.out.println(min(arr));
 
     }
     static int ls(int[] arr,int target){
@@ -15,5 +16,14 @@ public class linearsearch {
                  return i;}   
         }
         return -1;
+    }
+        static int min(int[] arr ){
+            int m=arr[0];
+            for(int i=1;i<arr.length;i++){
+            if(arr[i]<m){
+                m=arr[i];
+            }
+        }
+        return m;
     }
 }
