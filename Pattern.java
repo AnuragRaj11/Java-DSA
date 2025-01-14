11233b6a9e8d66a7fe1175690727d87b20a8c713
AnuragRaj11 <Araj59519@gmail.com>
2025-01-14 21:13:29 +0530

    Pattern

--- Pattern.java
+++ Pattern.java
@@ -1,1 +1,15 @@
+class Pattern {
+    public static void main(String[] args) {
+        p1(2);
+    }
 
+    static void p1(int n) {
+        for (int rows = 1; rows <= n; rows++) {
+
+            for (int cols = 1; cols <= rows; cols++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
