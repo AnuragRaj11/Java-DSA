0b039f6da35c344916b817d6ffdd546d7b1d6561
AnuragRaj11 <Araj59519@gmail.com>
2025-05-26 23:23:06 +0530

    recursion

--- Skiprec.java
+++ Skiprec.java
@@ -1,20 +1,23 @@
 public class Skiprec {
 
     public static void main(String[] args) {
-        String p = "baccad";
-        String result = skip("", p);
-        System.out.println(result); 
+        // String p = "baccad";
+      skip("", "abc");
     }
 
-    public static String skip(String processed, String unprocessed) {
+    public static void skip(String processed, String unprocessed) {
         if (unprocessed.isEmpty()) {
-            return processed;
+            System.out.println(processed);
+            return;
         }
         char ch = unprocessed.charAt(0);
-        if (ch == 'a') {
-            return skip(processed, unprocessed.substring(1));
-        } else {
-            return skip(processed + ch, unprocessed.substring(1));
-        }
+        // if (ch == 'a') {
+        //     return skip(processed, unprocessed.substring(1));
+        // } else {
+        //     return skip(processed + ch, unprocessed.substring(1));
+        // }
+
+         skip(processed, unprocessed.substring(1));
+         skip(processed + ch, unprocessed.substring(1));
     }
 }
