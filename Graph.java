90de46429f0c8ea5b9dae77641a6e8f79f008088
AnuragRaj11 <Araj59519@gmail.com>
2025-07-15 20:00:41 +0530

    BFS&DFS

--- Graph.java
+++ Graph.java
@@ -10,6 +10,13 @@ public static void main (String args[]) {
         g.addEdge(4, 5);
         
         printGraph(g);
+
+        System.out.println("BFS starting from node 1:");
+        g.bfs(1);
+        System.out.println("\nDFS starting from node 1:");
+        g.dfs(1);
+    System.out.println();
+    System.out.println("Graph created successfully.");
     }
 
     public void addEdge(int source, int destination) {
@@ -27,5 +34,39 @@ public static void main (String args[]) {
         for (Map.Entry<Integer, List<Integer>> entry : g.adjList.entrySet()) {
             System.out.println("Node " + entry.getKey() + " has neighbors: " + entry.getValue());
         }
+    }
+
+        public void bfs(int start) {
+            Set<Integer> visited = new HashSet<>();
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(start);
+            visited.add(start);
+
+            while (!queue.isEmpty()) {
+                int node = queue.poll();
+                System.out.print(node + " ");
+
+                for (int neighbor : getNeighbors(node)) {
+                    if (!visited.contains(neighbor)) {
+                        visited.add(neighbor);
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+
+    public void dfs(int start) {
+        Set<Integer> visited = new HashSet<>();
+        dfsHelper(start, visited);
+    }
+    private void dfsHelper(int node, Set<Integer> visited) {
+        visited.add(node);
+        System.out.print(node + " ");
+
+        for (int neighbor : getNeighbors(node)) {
+            if (!visited.contains(neighbor)) {
+                dfsHelper(neighbor, visited);
+            }
+        }
+    }
 }
-} 
